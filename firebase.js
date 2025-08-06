050cc1148480d97457afef408aca18a28e297b5c
Bruno Reis <Bruno.g.reis@gmail.com>
2025-08-05 22:53:10 -0300

    Update firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD3TI623gpV5xgvhGnEReCIa22YkSGYhAs",
+  authDomain: "chaveirocontabil.firebaseapp.com",
+  projectId: "chaveirocontabil",
+  storageBucket: "chaveirocontabil.firebasestorage.app",
+  messagingSenderId: "658638886556",
+  appId: "1:658638886556:web:ea3f6ff210afad8b7ebf59",
+  measurementId: "G-QF5JG8EYE9"
+};
